bb7b6bcf8eff567f064e2bd7008fc479c461d163
Grexjr <geerexed@gmail.com>
2025-07-25 22:23:18 -0400

    ROUND OVER DIALOG TEXT DISPLAY
    This commit displays all the text required for the round over dialog. The formatting is messed up which can be fixed later, but it is functional. The buttons are also hard coded here, which I don't really like,
    but that might just be the best option--besides, the only place yes or no comes up is here anyway, so it's fine for now and can be fixed later.
    
    Next, the action listeners need to be added to the buttons! I'd like for it to change some boolean in the round over dialog that the game controller reads; a continueGame boolean. That's the best option for sure.
    Then, GameController does its stuff based on that input.

--- src/main/tester.java
+++ src/main/tester.java
@@ -5,11 +5,17 @@ import controller.GameController;
 import obj.Player;
 import view.*;
 
+import java.util.ArrayList;
+
 public class tester {
 
     public static void main(String[] args){
         GameWindow window = new GameWindow();
         DifficultyDialog dialog = new DifficultyDialog(window.getFrame());
+        ArrayList<Integer> intList = new ArrayList<Integer>();
+        intList.add(3);
+        intList.add(5);
+        RoundOverDialog dialog2 = new RoundOverDialog(window.getFrame(),intList);
         Game gameState = new Game();
         Player player = new Player();
 
--- src/view/RoundOverDialog.java
+++ src/view/RoundOverDialog.java
@@ -2,22 +2,61 @@ package view;
 
 import javax.swing.*;
 import java.util.ArrayList;
-import java.util.Arrays;
 
+import static controller.Strings.GAME_QUESTIONS;
 
 
 public class RoundOverDialog extends GameDialog{
     // === CONSTANTS ===
-    private static final String ROUND_OVER_TITLE = "ROUND OVER!";
+    private static final String ROUND_OVER_TITLE = "ROUND OVER";
+    private static final String ROUND_OVER_TEXT = "Guess List:\n";
     private static final JPanel ROUND_OVER_BUTTONS = new JPanel();
-    // Do i have it as constant for here? I want to display the guess list on this, so probably not. pass as param
+    // Do I have it as constant for here? I want to display the guess list on this, so probably not. pass as param
 
 
     // === CONSTRUCTOR ===
     public RoundOverDialog(JFrame owner, ArrayList<Integer> guessList){
-        super(owner,ROUND_OVER_TITLE,guessList.toString(),ROUND_OVER_BUTTONS);
+        super(owner,ROUND_OVER_TITLE,ROUND_OVER_TEXT,ROUND_OVER_BUTTONS);
+        // SO this is technically creating two JLabels, the first with "Guess List:\n", the second with that AND the
+        // guess list... so I'll want to fix that. I may just have to overwrite the whole constructor from
+        // the game dialog, or find some way to genericize the formatting there (maybe have none).
+        // TODO: Remove formatting from game dialog superclass
+
+        displayGuessList(guessList);
 
         this.setVisible(true);
+    }
+
+    private void displayGuessList(ArrayList<Integer> guessList){
+        this.add(
+                new JLabel(
+                        ROUND_OVER_TEXT +
+                                guessList.toString()
+                                        .replace("[","")
+                                        .replace("]","")
+                                        .trim() + "\n" +
+                                GAME_QUESTIONS[1], //TODO: New JLabel for this and the other text that puts this bottom
+                        SwingConstants.CENTER
+                )
+        );
+    }
+
+    @Override
+    protected ArrayList<JButton> createButtons(){
+        JButton yes = new JButton("Yes");
+        yes.addActionListener(_ -> {
+
+        });
+
+        JButton no = new JButton("No");
+        no.addActionListener(_ -> {
+
+        });
+
+        ArrayList<JButton> buttonsList = new ArrayList<JButton>();
+        buttonsList.add(yes);
+        buttonsList.add(no);
+        return buttonsList;
     }
 
 
